4221d29d8fd2c108260d649a97801e8d52ff1672
Alexander <alexander.davidson87@gmail.com>
2021-08-09 15:17:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({
 }));
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
