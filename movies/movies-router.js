b6b1f73a16ad06152f80a3ea65ef5184cf5d36e0
Alexander <alexander.davidson87@gmail.com>
2021-08-16 13:24:26 -0500

    Update movies-router.js
    Allowed for updating movies

--- movies/movies-router.js
+++ movies/movies-router.js
@@ -54,19 +54,52 @@ MoviesRouter.get('/', (req, res) => { /* get request for all movies */
         res.status(500).send('Error ' + error);
       });
   })
-  .delete('/:Title', passport.authenticate('jwt', { session: false }), (req, res) => { /*  Deletes a movie from our list by title */
-    Movies.findOneAndRemove({ Title: req.params.Title })
-      .then((movie) => {
-        if (!movie) {
-          res.status(400).send(req.params.Title + ' was not found');
+  .put('/:Title', passport.authenticate('jwt', { session: false }), (req, res) => { /* update a movie */
+    let obj = {};
+    if (req.body.Title) {
+      obj.Title = req.body.Title
+    }
+    if (req.body.Description) {
+      obj.Description = Description
+    }
+    if (req.body.Director) {
+      obj.Director = req.body.Director
+    }
+    if (req.body.Genre) {
+      obj.Genre = req.body.Genre
+    }
+    if (req.body.ImagePath) {
+      obj.ImagePath = req.body.ImagePath
+    }
+    if (req.body.Actors) {
+      obj.Actors = req.body.Actors
+    }
+    if (req.body.Featured) {
+      obj.Featured = req.body.Featured
+    }
+    Movies.findOneAndUpdate({ Title: req.params.Title }, { $set: obj },
+      { new: true },
+      (err, updatedMovie) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
         } else {
-          res.status(200).send(req.params.Title + ' was deleted.');
+          res.json(updatedMovie);
         }
       })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
+      .delete('/:Title', passport.authenticate('jwt', { session: false }), (req, res) => { /*  Deletes a movie from our list by title */
+        Movies.findOneAndRemove({ Title: req.params.Title })
+          .then((movie) => {
+            if (!movie) {
+              res.status(400).send(req.params.Title + ' was not found');
+            } else {
+              res.status(200).send(req.params.Title + ' was deleted.');
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+          });
       });
-  });
 
-module.exports = MoviesRouter
+    module.exports = MoviesRouter
